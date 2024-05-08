d42d29b20102cd83442c98aafd16266e15fa981e
Ricardo Gutierrez <rick1304.25@gmail.com>
2024-05-08 16:49:05 -0600

    First commit

---
+++ store/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/your_database'
+
+mongoDB = PyMongo(app)
+
+from . import mongo_models
+
+from .views import *
