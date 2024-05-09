93a3cadc05ea65f4b8bfd5de00cf836636ecb1ed
unknown <dizzy.j4ck@gmail.com>
2024-05-09 00:05:40 -0600

    add the create_tag and modify the inventory controller

--- store/route.py
+++ store/route.py
@@ -1,20 +1,29 @@
 from flask import redirect, url_for, render_template, request
 from . import app
-                        
+
+tags = ["terror", "romance", "matemáticas", "shitpost", "física", "uwu", "derecho", "comercio", "quantica", "ciencia ficción"]
+
 @app.route('/', methods=['GET','POST'])
-def inventory_tags():
-    tags = ["terror", "romance", "matemáticas", "shitpost", "física", "uwu"]
+@app.route('/inventory', methods=['GET','POST'])
+def inventory():
+
     if request.method=='POST':
         # Handle POST Request here
         return render_template('inventory.html')        
 
     return render_template('inventory.html', tags=tags)         
 
+@app.route('/create_tag', methods=['GET','POST'])
+def create_tag():
+    if request.method =='POST':
+
+        tags.append(request.form['tagName'])
+        #redirect(url_for('inventory'))
+
+    return render_template('inventory.html', tags=tags)
 
 
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/costumer_view', methods=['GET', 'POST'])
 def costumer_view():
     tags = []
     if request.method=='POST':
