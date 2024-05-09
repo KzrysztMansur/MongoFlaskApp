c7798d43bb366dbc367c70d77e95f9809e7a3b47
unknown <dizzy.j4ck@gmail.com>
2024-05-08 19:01:39 -0600

    fix an error name

--- store/model.py
+++ store/model.py
@@ -1,4 +1,4 @@
-from store import mongo
+from store import mongoDB
 
 class Product:
     def __init__(self, name, description, image_url, tags):
@@ -13,11 +13,11 @@ class Product:
     
     @staticmethod
     def get_by_tags(tags):
-        return mongo.db.products.find({'tags.id': {'$in': tags}})
+        return mongoDB.db.products.find({'tags.id': {'$in': tags}})
 
 
     def save(self):
-        mongo.db.users.insert_one({
+        mongoDB.db.users.insert_one({
             'name': self.name,
             'description': self.description,
             'image_url': self.image_url
@@ -30,7 +30,7 @@ class Product:
         else:
             self.tags.append(new_tags)
 
-        mongo.db.products.update_one({'_id': self.id}, {'$set': {'tags': self.tags}})
+        mongoDB.db.products.update_one({'_id': self.id}, {'$set': {'tags': self.tags}})
 
 
 
@@ -41,17 +41,17 @@ class Tags:
 
     @staticmethod
     def get_tags_by_product(user_id):
-        return mongo.db.posts.find({'id': user_id})
+        return mongoDB.db.posts.find({'id': user_id})
     
  
     def update_tag_name(self, old_tag_name, new_tag_name):
 
-        mongo.db.tags.update_one({'id': self.id, 'tag': old_tag_name}, {'$set': {'tag': new_tag_name}})
+        mongoDB.db.tags.update_one({'id': self.id, 'tag': old_tag_name}, {'$set': {'tag': new_tag_name}})
         self.tag = new_tag_name  
 
 
     def save(self):
-        mongo.db.posts.insert_one({
+        mongoDB.db.posts.insert_one({
             'title': self.id,
             'content': self.tag,
         })
