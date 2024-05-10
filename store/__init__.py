ba6a2c81d307ebb505b58451af9f082cb4a06aab
unknown <dizzy.j4ck@gmail.com>
2024-05-09 23:06:49 -0600

    add the configurations for the uploading folder and the path to it

--- store/__init__.py
+++ store/__init__.py
@@ -3,8 +3,11 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'MongoFlaskApp\store\static\img_urls'
+
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/your_database'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 mongoDB = PyMongo(app)
 
