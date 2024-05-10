9f5effd2f314fa0170282303cb48e4c853aaf2b8
unknown <dizzy.j4ck@gmail.com>
2024-05-09 23:10:03 -0600

    minor changes to the code format

--- store/model.py
+++ store/model.py
@@ -1,8 +1,9 @@
 from store import mongoDB
 
 class Product:
-    def __init__(self, name, description, image_url, tags):
+    def __init__(self, name, price, description, image_url, tags):
         self.name = name
+        self.price = price
         self.description = description
         self.image_url = image_url
         self.tags = tags
@@ -15,14 +16,13 @@ class Product:
     def get_by_tags(tags):
         return mongoDB.db.products.find({'tags.id': {'$in': tags}})
 
-
     def save(self):
         mongoDB.db.users.insert_one({
             'name': self.name,
+            'price': self.price,
             'description': self.description,
             'image_url': self.image_url
         })
-
     
     def add_tags(self, new_tags):
         if isinstance(new_tags, list):
@@ -31,7 +31,6 @@ class Product:
             self.tags.append(new_tags)
 
         mongoDB.db.products.update_one({'_id': self.id}, {'$set': {'tags': self.tags}})
-
 
 
 class Tags:
@@ -43,12 +42,10 @@ class Tags:
     def get_tags_by_product(user_id):
         return mongoDB.db.posts.find({'id': user_id})
     
- 
     def update_tag_name(self, old_tag_name, new_tag_name):
 
         mongoDB.db.tags.update_one({'id': self.id, 'tag': old_tag_name}, {'$set': {'tag': new_tag_name}})
         self.tag = new_tag_name  
-
 
     def save(self):
         mongoDB.db.posts.insert_one({
