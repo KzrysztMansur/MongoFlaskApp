136ae6136550d118ca5b7dd8691abcdd81b8336d
Ricardo Gutierrez <rick1304.25@gmail.com>
2024-05-12 14:51:28 -0600

    Fix merge conflict

--- store/route.py
+++ store/route.py
@@ -3,14 +3,14 @@ from werkzeug.utils import secure_filename
 import os
 from . import app
 
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}  # Define the allowed file extensions
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-class Card:
+class Product:
     def __init__(self, img, name, price, description, tags_id):
         self.img = img
         self.name = name
@@ -21,9 +21,9 @@ class Card:
 
 tags = ["terror", "romance", "matemáticas", "shitpost", "física", "uwu", "derecho", "comercio", "quantica",
         "ciencia ficción"]
-cards = [Card(None, "maths for dummies", 70, "this is a book for people who dont know maths", [2, 3, 4, 5]),
-         Card(None, "php for dummies", 70, "this is a book for people who dont know php", [2, 3, 4, 5]),
-         Card(None, "web for dummies", 70, "this is a book for people who dont know web", [2, 3, 4, 5])
+cards = [Product(None, "maths for dummies", 70, "this is a book for people who dont know maths", [2, 3, 4, 5]),
+         Product(None, "php for dummies", 70, "this is a book for people who dont know php", [2, 3, 4, 5]),
+         Product(None, "web for dummies", 70, "this is a book for people who dont know web", [2, 3, 4, 5])
          ]
 
 
@@ -53,11 +53,22 @@ def add_product():
         price = request.form['price']
         description = request.form['description']
         selected_tags = request.form.getlist('tags[]')
+        file = request.files['productImage']
 
-        print(name)
-        print(price)
-        print(description)
-        print(selected_tags)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            name = request.form['name']
+            price = request.form['price']
+            description = request.form['description']
+            selected_tags = request.form.getlist('tags[]')
+
+            print(name)
+            print(price)
+            print(description)
+            print(selected_tags)
+            print('File saved as:', filename)
 
         return redirect(url_for('inventory'))
 
@@ -66,14 +77,13 @@ def add_product():
 
 @app.route('/costumer_view', methods=['GET', 'POST'])
 def costumer_view():
-    tags = []
     if request.method == 'POST':
         # aquí lo logica para que solamente regrese solo los objetos de los tags seleccionados
-        products = []
+        # products = []
 
-        return render_template('costumerview.html', tags=tags, products=products)
+        return render_template('costumerview.html', tags=tags, cards=cards)
 
     # aquí es para que regrese los objetos sin ningun tipo de filtro
-    products = []
+    # products = []
 
-    return render_template('costumerview.html', tags=tags, products=products)
+    return render_template('costumerview.html', tags=tags, cards=cards)
