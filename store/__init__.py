164114f6df7bc9d960ecad09be9c26c837722ee0
Ricardo Gutierrez <rick1304.25@gmail.com>
2024-05-12 15:32:27 -0600

    Rebuild model

--- store/__init__.py
+++ store/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask
-from flask_pymongo import PyMongo
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'MongoFlaskApp\store\static\img_urls'
 
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/your_database'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-mongoDB = PyMongo(app)
+mongo_client = MongoClient('localhost', 27017)
 
-from .route import *
+from .routes import *
--- store/model.py
+++ store/model.py
@@ -1,54 +1,153 @@
-from store import mongoDB
+from typing import List, Dict, Any, Optional
+from bson import ObjectId
 
 
-class Product:
-    def __init__(self, name, price, description, image_url, tags):
-        self.name = name
-        self.price = price
-        self.description = description
-        self.image_url = image_url
-        self.tags = tags
+class TagManager:
+    def __init__(self, db):
+        """
+        Initialize the TagManager with a MongoDB database.
 
-    @staticmethod
-    def get_by_name(name):
-        return mongoDB.db.users.find_one({'name': name})
+        :param db: MongoDB database instance
+        """
+        self.collection = db['tags']
 
-    @staticmethod
-    def get_by_tags(tags):
-        return mongoDB.db.products.find({'tags.id': {'$in': tags}})
+    def add_tag(self, name: str) -> ObjectId:
+        """
+        Add a new tag to the database.
 
-    def save(self):
-        mongoDB.db.users.insert_one({
-            'name': self.name,
-            'price': self.price,
-            'description': self.description,
-            'image_url': self.image_url
-        })
+        :param name: The name of the tag to add
+        :return: The ID of the created tag
+        """
+        return self.collection.insert_one({'name': name}).inserted_id
 
-    def add_tags(self, new_tags):
-        if isinstance(new_tags, list):
-            self.tags.extend(new_tags)
+    def get_tag_by_id(self, tag_id: ObjectId) -> Dict[str, Any]:
+        """
+        Retrieve a tag by its ID.
+
+        :param tag_id: The ID of the tag to retrieve
+        :return: The retrieved tag document
+        """
+        return self.collection.find_one({'_id': tag_id})
+
+    def get_all_tags(self) -> List[Dict[str, Any]]:
+        """
+        Retrieve all tags from the database.
+
+        :return: A list of all tag documents
+        """
+        return list(self.collection.find({}))
+
+    def update_tag(self, tag_id: ObjectId, new_name: str) -> Optional[bool]:
+        """
+        Update the name of an existing tag.
+
+        :param tag_id: The ID of the tag to update
+        :param new_name: The new name for the tag
+        :return: True if the update was successful, None otherwise
+        """
+        return self.collection.update_one({'_id': tag_id}, {'$set': {'name': new_name}}).modified_count
+
+    def delete_tag(self, tag_id: ObjectId) -> Optional[bool]:
+        """
+        Delete a tag by its ID.
+
+        :param tag_id: The ID of the tag to delete
+        :return: True if the deletion was successful, None otherwise
+        """
+        return self.collection.delete_one({'_id': tag_id}).deleted_count
+
+
+class ProductManager:
+    def __init__(self, db):
+        """
+        Initialize the ProductManager with a MongoDB database instance.
+
+        :param db: MongoDB database instance
+        """
+        self.collection = db['products']
+
+    def add_product(self, name: str, price: float, description: str, image_url: str,
+                    tags: Optional[List[ObjectId]] = None) -> ObjectId:
+        """
+        Add a new product to the database.
+
+        :param name: The name of the product
+        :param price: The price of the product
+        :param description: The description of the product
+        :param image_url: The URL of the product image
+        :param tags: Optional; List of tag IDs associated with the product. Can be an empty list.
+        :return: The ID of the created product
+        """
+        return self.collection.insert_one({
+            'name': name,
+            'price': price,
+            'description': description,
+            'image_url': image_url,
+            'tags': tags if tags else []
+        }).inserted_id
+
+    def get_product_by_id(self, product_id: ObjectId) -> Dict[str, Any]:
+        """
+        Retrieve a product by its ID.
+
+        :param product_id: The ID of the product to retrieve
+        :return: The retrieved product document
+        """
+        return self.collection.find_one({'_id': product_id})
+
+    def get_all_products(self) -> List[Dict[str, Any]]:
+        """
+        Retrieve all products from the database.
+
+        :return: A list of all product documents
+        """
+        return list(self.collection.find({}))
+
+    def update_product(self, product_id: ObjectId, name: Optional[str] = None, price: Optional[float] = None,
+                       description: Optional[str] = None, image_url: Optional[str] = None,
+                       tags: Optional[List[ObjectId]] = None) -> Optional[bool]:
+        """
+        Update the information of an existing product.
+
+        :param product_id: The ID of the product to update
+        :param name: Optional; The new name of the product
+        :param price: Optional; The new price of the product
+        :param description: Optional; The new description of the product
+        :param image_url: Optional; The new image URL of the product
+        :param tags: Optional; The new list of tag IDs for the product
+        :return: True if the update was successful, None otherwise
+        """
+        update_data = {k: v for k, v in locals().items() if v is not None and k != 'product_id'}
+        return self.collection.update_one({'_id': product_id}, {'$set': update_data}).modified_count
+
+    def delete_product(self, product_id: ObjectId) -> Optional[bool]:
+        """
+        Delete a product by its ID.
+
+        :param product_id: The ID of the product to delete
+        :return: True if the deletion was successful, None otherwise
+        """
+        return self.collection.delete_one({'_id': product_id}).deleted_count
+
+    def find_products_by_tag(self, tag_id: ObjectId) -> List[Dict[str, Any]]:
+        """
+        Find all products that contain a specific tag.
+
+        :param tag_id: The ID of the tag to search by
+        :return: A list of products that contain the specified tag
+        """
+        return list(self.collection.find({'tags': tag_id}))
+
+    def find_products_by_tags(self, tags: List[ObjectId], match_all: bool = False) -> List[Dict[str, Any]]:
+        """
+        Find products based on a list of tags.
+
+        :param tags: List of tag IDs to search by
+        :param match_all: True to require all tags in each product, False to require at least one tag
+        :return: A list of products that match the tag criteria
+        """
+        if match_all:
+            query = {'tags': {'$all': tags}}
         else:
-            self.tags.append(new_tags)
-
-        mongoDB.db.products.update_one({'_id': self.id}, {'$set': {'tags': self.tags}})
-
-
-class Tags:
-    def __init__(self, id, tag):
-        self.id = id
-        self.tag = tag
-
-    @staticmethod
-    def get_tags_by_product(user_id):
-        return mongoDB.db.posts.find({'id': user_id})
-
-    def update_tag_name(self, old_tag_name, new_tag_name):
-        mongoDB.db.tags.update_one({'id': self.id, 'tag': old_tag_name}, {'$set': {'tag': new_tag_name}})
-        self.tag = new_tag_name
-
-    def save(self):
-        mongoDB.db.posts.insert_one({
-            'title': self.id,
-            'content': self.tag,
-        })
+            query = {'tags': {'$in': tags}}
+        return list(self.collection.find(query))
